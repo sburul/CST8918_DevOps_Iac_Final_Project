bd64cf81d90a51ba0bb05a3a5caf3fd085e4528e
Serpil-Dndr <serpil.dndrca@gmail.com>
2025-04-14 15:07:53 -0400

    added al necessary code to the main tf and outputs tf

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -20,3 +20,22 @@ module "aks" {
   test_subnet_id      = module.network.test_subnet_id
   prod_subnet_id      = module.network.prod_subnet_id
 }
+# Redis Cache Module
+module "redis" {
+  source              = "./modules/redis_cache"
+  label_prefix        = var.labelPrefix
+  location            = var.region
+  resource_group_name = azurerm_resource_group.rg.name
+  redis_cache_prefix  = "weather"
+  # Default values from existing module
+  redis_cache_sku                     = "Basic"
+  redis_cache_family                  = "C"
+  redis_cache_capacity_test           = 0
+  redis_cache_capacity_prod           = 1
+  redis_public_network_access_enabled = true
+  redis_enable_authentication         = true
+  vnet_id                             = module.network.vnet_id
+  vnet_name                           = module.network.vnet_name
+  subnet_id_test                      = module.network.test_subnet_id
+  subnet_id_prod                      = module.network.prod_subnet_id
+}
