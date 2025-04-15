2ad6e47028568309f63f6c0c32de2b732676fdb0
Seyfullah Burul <75156011+sburul@users.noreply.github.com>
2025-04-14 22:54:45 -0400

    Feature/GitHub workflows (#14)
    * feat: Remix Weather App module created
    
    * doc: Readme file updated for remix-weather-app
    
    * feat:Build and Push Docker Image to Azure ACR
    
    * fix:Yaml file corrected
    
    * fix:Update Github actions files
    
    * fix:Github actions files added
    
    * fix:Terraform files fixed
    
    * fix:Github actions - Terraform Plan file deleted
    
    * fix:Github actions - Terraform Plan file deleted
    
    * fix:Terraform Remix Weather App deployment config updated
    
    ---------
    
    Co-authored-by: Gulsah Avsar <133474869+GulsahAvsar@users.noreply.github.com>

--- infra/tf-app/modules/redis_cache/main.tf
+++ infra/tf-app/modules/redis_cache/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.25.0"  # Specifies the Azure RM provider version
+      version = "~> 4.25.0" # Specifies the Azure RM provider version
     }
   }
 }
@@ -10,13 +10,13 @@ terraform {
 # Define Azure Redis Cache for Test environment
 resource "azurerm_redis_cache" "redis_test" {
   name                          = lower("redis-${var.label_prefix}-test") # Redis Cache name (prefix + label + test)
-  resource_group_name           = var.resource_group_name # Name of the resource group where Redis will be deployed
-  location                      = var.location # Azure region where Redis will be deployed
-  capacity                      = 1 # Redis capacity (valid values are 0-6 for Basic/Standard SKU, 1-5 for Premium SKU)
-  family                        = "C" # SKU family: C (Basic/Standard)
-  sku_name                      = "Basic" # SKU type: Basic
-  non_ssl_port_enabled          = false # Disable non-SSL port
-  minimum_tls_version           = "1.2" # Minimum TLS version: 1.2
+  resource_group_name           = var.resource_group_name                 # Name of the resource group where Redis will be deployed
+  location                      = var.location                            # Azure region where Redis will be deployed
+  capacity                      = 1                                       # Redis capacity (valid values are 0-6 for Basic/Standard SKU, 1-5 for Premium SKU)
+  family                        = "C"                                     # SKU family: C (Basic/Standard)
+  sku_name                      = "Basic"                                 # SKU type: Basic
+  non_ssl_port_enabled          = false                                   # Disable non-SSL port
+  minimum_tls_version           = "1.2"                                   # Minimum TLS version: 1.2
   public_network_access_enabled = var.redis_public_network_access_enabled # Whether public network access is allowed
 
   redis_configuration {
@@ -27,13 +27,13 @@ resource "azurerm_redis_cache" "redis_test" {
 # Define Azure Redis Cache for Production environment
 resource "azurerm_redis_cache" "redis_prod" {
   name                          = lower("redis-${var.label_prefix}-prod") # Redis Cache name (prefix + label + prod)
-  resource_group_name           = var.resource_group_name # Name of the resource group where Redis will be deployed
-  location                      = var.location # Azure region where Redis will be deployed
-  capacity                      = 1 # Redis capacity (valid values are 0-6 for Basic/Standard SKU, 1-5 for Premium SKU)
-  family                        = "C" # SKU family: C (Basic/Standard)
-  sku_name                      = "Basic" # SKU type: Basic
-  non_ssl_port_enabled          = false # Disable non-SSL port
-  minimum_tls_version           = "1.2" # Minimum TLS version: 1.2
+  resource_group_name           = var.resource_group_name                 # Name of the resource group where Redis will be deployed
+  location                      = var.location                            # Azure region where Redis will be deployed
+  capacity                      = 1                                       # Redis capacity (valid values are 0-6 for Basic/Standard SKU, 1-5 for Premium SKU)
+  family                        = "C"                                     # SKU family: C (Basic/Standard)
+  sku_name                      = "Basic"                                 # SKU type: Basic
+  non_ssl_port_enabled          = false                                   # Disable non-SSL port
+  minimum_tls_version           = "1.2"                                   # Minimum TLS version: 1.2
   public_network_access_enabled = var.redis_public_network_access_enabled # Whether public network access is allowed
 
   redis_configuration {
@@ -44,34 +44,34 @@ resource "azurerm_redis_cache" "redis_prod" {
 # Create Azure Private DNS Zone for Redis
 resource "azurerm_private_dns_zone" "pdz_redis" {
   name                = "privatelink.redis.cache.windows.net" # Private DNS zone name for Redis
-  resource_group_name = var.resource_group_name # Resource group for the DNS zone
+  resource_group_name = var.resource_group_name               # Resource group for the DNS zone
 }
 
 # Create a link between the Private DNS Zone and VNet
 resource "azurerm_private_dns_zone_virtual_network_link" "redis_pdz_vnet_link" {
-  name                  = "privatelink_to_${var.vnet_name}" # Name of the link between the VNet and the DNS zone
-  resource_group_name   = var.resource_group_name # Resource group for the DNS zone
-  virtual_network_id    = var.vnet_id # VNet ID for the link
+  name                  = "privatelink_to_${var.vnet_name}"       # Name of the link between the VNet and the DNS zone
+  resource_group_name   = var.resource_group_name                 # Resource group for the DNS zone
+  virtual_network_id    = var.vnet_id                             # VNet ID for the link
   private_dns_zone_name = azurerm_private_dns_zone.pdz_redis.name # Name of the DNS zone to link
 }
 
 # Create a Private Endpoint for Azure Redis Cache in the Test environment
 resource "azurerm_private_endpoint" "pe_redis_test" {
   name                = lower("${var.private_endpoint_prefix}-${azurerm_redis_cache.redis_test.name}") # Private Endpoint name
-  location            = azurerm_redis_cache.redis_test.location # Location of the Redis test environment
-  resource_group_name = azurerm_redis_cache.redis_test.resource_group_name # Resource group for the Redis test environment
-  subnet_id           = var.subnet_id_test # Subnet ID for the test environment's private endpoint
+  location            = azurerm_redis_cache.redis_test.location                                        # Location of the Redis test environment
+  resource_group_name = azurerm_redis_cache.redis_test.resource_group_name                             # Resource group for the Redis test environment
+  subnet_id           = var.subnet_id_test                                                             # Subnet ID for the test environment's private endpoint
 
   private_service_connection {
     name                           = "pe-${azurerm_redis_cache.redis_test.name}" # Name of the private service connection
-    private_connection_resource_id = azurerm_redis_cache.redis_test.id # Redis Cache resource ID
-    is_manual_connection           = false # Whether the connection is manual
-    subresource_names              = ["redisCache"] # Subresource to connect to (Redis cache)
-    request_message                = try(var.request_message, null) # Optional message for connection request
+    private_connection_resource_id = azurerm_redis_cache.redis_test.id           # Redis Cache resource ID
+    is_manual_connection           = false                                       # Whether the connection is manual
+    subresource_names              = ["redisCache"]                              # Subresource to connect to (Redis cache)
+    request_message                = try(var.request_message, null)              # Optional message for connection request
   }
 
   private_dns_zone_group {
-    name                 = "default" # DNS zone group name
+    name                 = "default"                               # DNS zone group name
     private_dns_zone_ids = [azurerm_private_dns_zone.pdz_redis.id] # Private DNS zone ID
   }
 
@@ -84,20 +84,20 @@ resource "azurerm_private_endpoint" "pe_redis_test" {
 # Create a Private Endpoint for Azure Redis Cache in the Production environment
 resource "azurerm_private_endpoint" "pe_redis_prod" {
   name                = lower("${var.private_endpoint_prefix}-${azurerm_redis_cache.redis_prod.name}") # Private Endpoint name
-  location            = azurerm_redis_cache.redis_prod.location # Location of the Redis production environment
-  resource_group_name = azurerm_redis_cache.redis_prod.resource_group_name # Resource group for the Redis production environment
-  subnet_id           = var.subnet_id_prod # Subnet ID for the production environment's private endpoint
+  location            = azurerm_redis_cache.redis_prod.location                                        # Location of the Redis production environment
+  resource_group_name = azurerm_redis_cache.redis_prod.resource_group_name                             # Resource group for the Redis production environment
+  subnet_id           = var.subnet_id_prod                                                             # Subnet ID for the production environment's private endpoint
 
   private_service_connection {
     name                           = "pe-${azurerm_redis_cache.redis_prod.name}" # Name of the private service connection
-    private_connection_resource_id = azurerm_redis_cache.redis_prod.id # Redis Cache resource ID
-    is_manual_connection           = false # Whether the connection is manual
-    subresource_names              = ["redisCache"] # Subresource to connect to (Redis cache)
-    request_message                = try(var.request_message, null) # Optional message for connection request
+    private_connection_resource_id = azurerm_redis_cache.redis_prod.id           # Redis Cache resource ID
+    is_manual_connection           = false                                       # Whether the connection is manual
+    subresource_names              = ["redisCache"]                              # Subresource to connect to (Redis cache)
+    request_message                = try(var.request_message, null)              # Optional message for connection request
   }
 
   private_dns_zone_group {
-    name                 = "default" # DNS zone group name
+    name                 = "default"                               # DNS zone group name
     private_dns_zone_ids = [azurerm_private_dns_zone.pdz_redis.id] # Private DNS zone ID
   }
 
--- infra/tf-app/modules/remix_weather_app/main.tf
+++ infra/tf-app/modules/remix_weather_app/main.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
 }
-
+ 
 # ACR Configs
 resource "azurerm_container_registry" "acr" {
   name                = "${replace(var.label_prefix, "-", "")}acr"
@@ -15,7 +15,7 @@ resource "azurerm_container_registry" "acr" {
   sku                 = "Basic"
   admin_enabled       = true
 }
-
+ 
 # We give AKS the necessary permissions to take images from ACR
 resource "azurerm_role_assignment" "acr_pull_test" {
   principal_id                     = var.test_cluster_principal_id
@@ -23,14 +23,14 @@ resource "azurerm_role_assignment" "acr_pull_test" {
   scope                            = azurerm_container_registry.acr.id
   skip_service_principal_aad_check = true
 }
-
+ 
 resource "azurerm_role_assignment" "acr_pull_prod" {
   principal_id                     = var.prod_cluster_principal_id
   role_definition_name             = "AcrPull"
   scope                            = azurerm_container_registry.acr.id
   skip_service_principal_aad_check = true
 }
-
+ 
 # Kubernetes for Test Environment
 provider "kubernetes" {
   alias                  = "test"
@@ -39,8 +39,8 @@ provider "kubernetes" {
   client_key             = var.test_client_key
   cluster_ca_certificate = var.test_cluster_ca_certificate
 }
-
-# Kubernetes Config for 
+ 
+# Kubernetes Config for
 provider "kubernetes" {
   alias                  = "prod"
   host                   = var.prod_cluster_host
@@ -48,84 +48,84 @@ provider "kubernetes" {
   client_key             = var.prod_client_key
   cluster_ca_certificate = var.prod_cluster_ca_certificate
 }
-
+ 
 # Kubernetes Secret for Test Environment
 resource "kubernetes_secret" "remix_weather_app_secret_test" {
   provider = kubernetes.test
-  
+ 
   metadata {
-    name = "remix-weather-app-secrets"
+    name      = "remix-weather-app-secrets"
     namespace = "default"
   }
-
+ 
   data = {
     WEATHER_API_KEY = var.weather_api_key
-    REDIS_URL = "redis://:${var.test_redis_key}@${var.test_redis_host}:${var.test_redis_port}?tls=true"
+    REDIS_URL       = "redis://:${var.test_redis_key}@${var.test_redis_host}:${var.test_redis_port}?tls=true"
   }
 }
-
+ 
 # Kubernetes Secret for Production Environment
 resource "kubernetes_secret" "remix_weather_app_secret_prod" {
   provider = kubernetes.prod
-  
+ 
   metadata {
-    name = "remix-weather-app-secrets"
+    name      = "remix-weather-app-secrets"
     namespace = "default"
   }
-
+ 
   data = {
     WEATHER_API_KEY = var.weather_api_key
-    REDIS_URL = "redis://:${var.prod_redis_key}@${var.prod_redis_host}:${var.prod_redis_port}?tls=true"
+    REDIS_URL       = "redis://:${var.prod_redis_key}@${var.prod_redis_host}:${var.prod_redis_port}?tls=true"
   }
 }
-
+ 
 # Kubernetes Deployment for Test Environment
 resource "kubernetes_deployment" "remix_weather_app_test" {
   provider = kubernetes.test
-
+ 
   metadata {
-    name = "remix-weather-app"
+    name      = "remix-weather-app"
     namespace = "default"
     labels = {
       app = "remix-weather-app"
     }
   }
-
+ 
   spec {
     replicas = 1
-
+ 
     selector {
       match_labels = {
         app = "remix-weather-app"
       }
     }
-
+ 
     template {
       metadata {
         labels = {
           app = "remix-weather-app"
         }
       }
-
+ 
       spec {
         container {
           image = "${azurerm_container_registry.acr.login_server}/remix-weather-app:${var.image_tag}"
           name  = "remix-weather-app"
-
+ 
           port {
             container_port = var.container_port
           }
-
+ 
           env {
             name = "WEATHER_API_KEY"
             value_from {
               secret_key_ref {
-                name = "remix-weather-app-scdecrets"
+                name = "remix-weather-app-secrets"
                 key  = "WEATHER_API_KEY"
               }
             }
           }
-
+ 
           env {
             name = "REDIS_URL"
             value_from {
@@ -135,7 +135,7 @@ resource "kubernetes_deployment" "remix_weather_app_test" {
               }
             }
           }
-          
+ 
           resources {
             requests = {
               cpu    = "100m"
@@ -146,72 +146,72 @@ resource "kubernetes_deployment" "remix_weather_app_test" {
               memory = "256Mi"
             }
           }
-        }        
+        }
       }
     }
   }
 }
-
+ 
 # Kubernetes Service for Test Environment
 resource "kubernetes_service" "remix_weather_app_test" {
   provider = kubernetes.test
-
+ 
   metadata {
-    name = "remix-weather-app"
+    name      = "remix-weather-app"
     namespace = "default"
   }
-
+ 
   spec {
     selector = {
       app = "remix-weather-app"
     }
-    
+ 
     port {
       port        = 80
       target_port = 3000
     }
-
+ 
     type = "ClusterIP"
   }
 }
-
+ 
 # Kubernetes Deployment for Production Environment
 resource "kubernetes_deployment" "remix_weather_app_prod" {
   provider = kubernetes.prod
-
+ 
   metadata {
-    name = "remix-weather-app"
+    name      = "remix-weather-app"
     namespace = "default"
     labels = {
       app = "remix-weather-app"
     }
   }
-
+ 
   spec {
     replicas = 1
-
+ 
     selector {
       match_labels = {
         app = "remix-weather-app"
       }
     }
-
+ 
     template {
       metadata {
         labels = {
           app = "remix-weather-app"
         }
       }
-
+ 
       spec {
         container {
           image = "${azurerm_container_registry.acr.login_server}/remix-weather-app:${var.image_tag}"
           name  = "remix-weather-app"
-
+ 
           port {
             container_port = var.container_port
           }
-
+ 
           env {
             name = "WEATHER_API_KEY"
             value_from {
@@ -221,7 +221,7 @@ resource "kubernetes_deployment" "remix_weather_app_prod" {
               }
             }
           }
-
+ 
           env {
             name = "REDIS_URL"
             value_from {
@@ -231,7 +231,7 @@ resource "kubernetes_deployment" "remix_weather_app_prod" {
               }
             }
           }
-          
+ 
           resources {
             requests = {
               cpu    = "100m"
@@ -247,26 +247,28 @@ resource "kubernetes_deployment" "remix_weather_app_prod" {
     }
   }
 }
-
+ 
 # Kubernetes Service for Production Environment
 resource "kubernetes_service" "remix_weather_app_prod" {
   provider = kubernetes.prod
-
+ 
   metadata {
-    name = "remix-weather-app"
+    name      = "remix-weather-app"
     namespace = "default"
   }
-
+ 
   spec {
     selector = {
       app = "remix-weather-app"
     }
-    
+ 
     port {
       port        = 80
       target_port = 3000
     }
-
+ 
     type = "LoadBalancer"
   }
-} 
+}
+ 
+ 
